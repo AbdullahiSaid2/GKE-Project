f8c74ffe45772a6c88751dc54b6b1c4f208b8f03
AbdullahiSaid2 <abdulls018@gmail.com>
2022-08-26 16:40:09 +0000

    2nd commit

---
+++ nodeback/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const conn = require("./models/db");
+var corsOptions = {
+  origin: "*",
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+app.post("/adduser", (req, res) => {
+  const values = [req.body.firstName, req.body.lastName];
+  conn.query(
+    "INSERT INTO users (firstName, lastName) VALUES(?)",
+    [values],
+    function (err, data, fields) {
+      if (err) return next(new AppError(err, 500));
+      res.status(201).json({
+        status: "success",
+        message: "User has been added Successfully",
+      });
+    }
+  );
+});
+
+const PORT = process.env.PORT || 3306;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
---
+++ nodeback/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "localhost",
+  USER: "sql-user",
+  PASSWORD: "Password123",
+  DB: "mysql-2",
+};
